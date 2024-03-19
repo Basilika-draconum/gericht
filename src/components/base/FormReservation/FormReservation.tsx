d5c0b9e8c2ccf132f35ca933f2fc95074447b032
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-03-19 14:12:43 +0200

    add example for form

--- src/components/base/FormReservation/FormReservation.tsx
+++ src/components/base/FormReservation/FormReservation.tsx
@@ -6,9 +6,22 @@ export const FormReservation: React.FC<FormReservationProps> = ({ button }) => {
   return (
     <div className="flex flex-col items-center gap-16">
       <form className="flex flex-row gap-8 ">
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        {/* here will be form */}
+        <input
+          type="text"
+          className="border-text-addaccent border p-4 bg-bg-main text-[16px] font-upright font-semibold leading-[1.75] tracking-[0.64px] text-text-secondary"
+          placeholder="1 Person"
+        />
+        <input
+          type="text"
+          className="border-text-addaccent border p-4 bg-bg-main text-[16px] font-upright font-semibold leading-[1.75] tracking-[0.64px] text-text-secondary"
+          placeholder="1 Person"
+        />
+        <input
+          type="text"
+          className="border-text-addaccent border p-4 bg-bg-main text-[16px] font-upright font-semibold leading-[1.75] tracking-[0.64px] text-text-secondary"
+          placeholder="1 Person"
+        />
       </form>
 
       <Button type="submit" text={button} />
