d3e1247b9204823a6180695019700238acf5574a
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-03-25 19:39:18 +0200

    add section poison

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { About } from "@/sections/About";
 import { Contacts } from "@/sections/Contacts";
 import { Reservation } from "@/sections/Reservation";
 import { Special } from "@/sections/Special";
+import { Poison } from "@/sections/Poison/Poison";
 
 export default async function Home() {
   return (
     <>
       <Reservation />
       <About />
+      <Poison />
       <Special />
       <Contacts />
     </>
