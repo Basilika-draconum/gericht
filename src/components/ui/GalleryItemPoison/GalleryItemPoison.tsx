d878c85d7c50d8eee35ee93c4f06a630414fa2be
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-03-26 16:31:32 +0200

    add transition to component

--- src/components/ui/GalleryItemPoison/GalleryItemPoison.tsx
+++ src/components/ui/GalleryItemPoison/GalleryItemPoison.tsx
@@ -15,7 +15,7 @@ export const GalleryItemPoison: React.FC<IGalleryItemPoisonProps> = ({
         height={600}
         className="object-contain"
       />
-      <p className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-20 bg-text-extra group-hover:opacity-40 opacity-0  group-hover:text-text-white text-8 xl:text-[45px] font-semibold tracking-[1.8px] leading-[58.5] capitalize">
+      <p className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-20 transition-all bg-text-extra group-hover:opacity-40 opacity-0  group-hover:text-text-white text-8 xl:text-[45px] font-semibold tracking-[1.8px] leading-[58.5] capitalize">
         {title}
       </p>
     </>
