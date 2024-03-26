3395f0eceaf31d3775893e382ac18d93455c1e58
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-03-26 20:07:15 +0200

    add style to happyHourse

--- src/sections/HappyHours/HappyHours.tsx
+++ src/sections/HappyHours/HappyHours.tsx
@@ -12,7 +12,7 @@ export const HappyHours: React.FC = () => {
           src={image.src}
           alt={image.alt}
           width={1440}
-          height={800}
+          height={900}
           quality={100}
           sizes="100vw"
           style={{
@@ -20,9 +20,11 @@ export const HappyHours: React.FC = () => {
           }}
         />
       </div>
-      <div className="absolute ">
-        <h2 className="text-center mb-8">{title}</h2>
-        <h3 className="text-center">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <h2 className="w-full text-text-accent not-italic capitalize tracking-[3.84px] font-semibold text-9xl leading-[166.4px] font-upright text-center mb-8">
+          {title}
+        </h2>
+        <h3 className="w-full text-text-white not-italic capitalize tracking-[1.28px] font-bold leading-[41.6px] text-[32px] font-upright text-center">
           {day}
           {time}
         </h3>
