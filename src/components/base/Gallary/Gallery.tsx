188899c10f1145d27917ef4abd6b6c987692a652
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-03-27 19:07:59 +0200

    fix style to gallary images

--- src/components/base/Gallary/Gallery.tsx
+++ src/components/base/Gallary/Gallery.tsx
@@ -10,7 +10,7 @@ export const Gallery: React.FC = () => {
       {images.map(({ src, title, id }) => (
         <li
           key={id}
-          className="relative group transition-all cursor-pointer border border-solid border-bg-accent  overflow-hidden w-[250px] h-auto xl:w-[412px] xl:h-[600px]"
+          className="relative group transition-all cursor-pointer border border-solid border-bg-accent  overflow-hidden w-[250px] h-auto md:w-1/3  xl:w-[412px] xl:h-[600px]"
         >
           <GalleryItemPoison src={src} title={title} />
         </li>
