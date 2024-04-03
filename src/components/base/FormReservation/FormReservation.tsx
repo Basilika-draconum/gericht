41cb9191c9905096d04d25a9a45ab4246ff7e0d9
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-04-03 12:37:06 +0300

    fix select for form

--- src/components/base/FormReservation/FormReservation.tsx
+++ src/components/base/FormReservation/FormReservation.tsx
@@ -1,59 +1,147 @@
 "use client";
-
-import { Controller, useForm } from "react-hook-form";
-import { MenuItem, Select } from "@mui/material";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { MenuItem, Select, ThemeProvider } from "@mui/material";
+import toast from "react-hot-toast";
 // import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 // import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 // import { LocalizationProvider } from "@mui/x-date-pickers";
+// import dayjs, { Dayjs } from "dayjs";
 
+import { muiTheme } from "@/app/mui-theme";
 import { Button } from "@/components/ui/Button";
 
 import data from "@/data/reservation.json";
-// import dayjs, { Dayjs } from "dayjs";
 
 const defaultValues = {
-  select: "",
+  select1: "",
+  select2: "",
   // date: Dayjs,
 };
+interface FormData {
+  select1: string;
+  select2: string;
+}
 
 export const FormReservation: React.FC = () => {
-  const { button, guests } = data;
+  const { button, guests, times } = data;
   const {
     handleSubmit,
     control,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({ defaultValues });
 
-    // formState: { errors },
-  } = useForm({ defaultValues });
-
-  const onSubmit = handleSubmit((data) => console.log(data));
+  const onSubmit: SubmitHandler<FormData> = async (formData) => {
+    toast(
+      `You booked a table for ${formData.select1} people and at ${formData.select2}:00`,
+    );
+    reset();
+  };
 
   return (
     <div className="flex flex-col items-center gap-16">
-      <form className="flex flex-col xl:flex-row gap-8" onSubmit={onSubmit}>
-        <Controller
-          render={({ field }) => (
-            <Select
-              {...field}
-              size="medium"
-              sx={{ minWidth: 320 }}
-              className="border-text-addaccent border"
-            >
-              {guests.map((guest) => (
-                <MenuItem
-                  className="border-text-addaccent border bg-bg-main text-[13px] md:text-[20px] font-upright font-semibold leading-[1.75] tracking-[0.64px] text-text-secondary"
-                  value={guest.value}
-                  key={guest.value}
+      <ThemeProvider theme={muiTheme}>
+        <form
+          action="#"
+          className="flex flex-col items-center xl:flex-row gap-8"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col w-full relative">
+            <Controller
+              control={control}
+              name="select1"
+              defaultValue={"1"}
+              rules={{ required: "This field is required" }}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  size="medium"
+                  sx={{ minWidth: 320, color: "#F5EFDB" }}
+                  MenuProps={{
+                    PaperProps: {
+                      sx: {
+                        backgroundColor: "#0C0B08", // Колір фону спливаючого вікна
+                      },
+                    },
+                  }}
+                  className="border-text-addaccent border"
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
                 >
-                  {guest.label}
-                </MenuItem>
-              ))}
-            </Select>
-          )}
-          control={control}
-          name="select"
-          defaultValue={"10"}
-        />
-        {/* <Controller
+                  <MenuItem value="" className="text-text-secondary" disabled>
+                    <em>Number of people</em>
+                  </MenuItem>
+                  {guests.map((guest) => (
+                    <MenuItem
+                      className="text-[13px] md:text-[20px] font-upright font-semibold leading-[1.75] tracking-[0.64px] text-text-secondary"
+                      value={guest.value}
+                      key={guest.value}
+                    >
+                      {guest.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            />
+            <div className="flex justify-end absolute bottom-[-22px] right-0">
+              {errors?.select1 && (
+                <div className="flex fle-row items-center">
+                  <p className="text-rose-400 text-[13px] leading-6 tracking-[2.4px] ml-1">
+                    {errors?.select1?.message}
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full relative">
+            <Controller
+              control={control}
+              name="select2"
+              defaultValue={"11"}
+              rules={{ required: "This field is required" }}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  size="medium"
+                  sx={{ minWidth: 320, color: "#F5EFDB" }}
+                  MenuProps={{
+                    PaperProps: {
+                      sx: {
+                        backgroundColor: "#0C0B08", // Колір фону спливаючого вікна
+                      },
+                    },
+                  }}
+                  className="border-text-addaccent border relative"
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value="" className="text-text-secondary" disabled>
+                    <em>Time</em>
+                  </MenuItem>
+                  {times.map((time) => (
+                    <MenuItem
+                      className="text-[13px] md:text-[20px] font-upright font-semibold leading-[1.75] tracking-[0.64px] text-text-secondary"
+                      value={time.value}
+                      key={time.value}
+                    >
+                      {time.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            />
+            <div className="flex justify-end absolute bottom-[-22px] right-0">
+              {errors?.select2 && (
+                <div className="flex fle-row items-center">
+                  <p className="text-rose-400 text-[13px] leading-6 tracking-[2.4px] ml-1">
+                    {errors?.select2?.message}
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+          {/* <Controller
           name="date"
           control={control}
           defaultValue={dayjs()} // Початкове значення дати
@@ -66,9 +154,9 @@ export const FormReservation: React.FC = () => {
             </LocalizationProvider>
           )}
         /> */}
-      </form>
-
-      <Button type="submit" text={button} />
+          <Button type="submit" text={button} />
+        </form>
+      </ThemeProvider>
     </div>
   );
 };
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -8,7 +8,7 @@ export const Button: React.FC<ButtonProps> = ({
   return (
     <button
       type={type}
-      className={`w-fit bg-bg-accent py-2 px-8 text-text-extra font-upright font-bold text-[16px] leading-[1.75] tracking-[0.64px] transition-all hover:bg-bg-hoveraccent focus:bg-bg-hoveraccent cursor-pointer ${className}`}
+      className={`w-fit bg-bg-accent py-2 px-8 text-text-extra font-upright font-bold text-[16px] leading-[1.75] tracking-[0.64px] transition-all hover:bg-bg-hoveraccent focus:bg-bg-hoveraccent text-nowrap cursor-pointer ${className}`}
     >
       {text}
     </button>
